1dac26fb58fa093c04ec67fd7f22e4f72d3aabc3
jose <90952627+joseenricoaquino@users.noreply.github.com>
2025-10-19 11:45:55 +0800

    mongoose setup crud 2 challenge not done

--- myApp.js
+++ myApp.js
@@ -1,54 +1,121 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
+let validator = require('validator');
 
+mongoose.connect(process.env.MONGO_URI);
 
-let Person;
+const Schema = mongoose.Schema;
+
+const PersonSchema = new Schema({
+  name: {
+    type: String,
+    // required: true,
+    // unique: true,
+  },
+  age: Number,
+  favoriteFoods: [String]
+})
+
+const Person = mongoose.model("Person", PersonSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
-};
+  const person = new Person({
+    name: "playa", age: 24, favoriteFoods: ["lemon", "coke", "royal"]
+  })
+
+  person.save((err, data) => {
+    if(err) return console.error(err)
+    done(null, data)
+  })
+}
+
+ const arrayOfPeople = [
+    { name: "jake", age: 14, favoriteFoods: ["fries", "donut"]},
+    { name: "nathalia", age: 22, favoriteFoods: ["hotdog", "burger"]}
+  ];
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, people) => {
+    if (err) return console.log(err)
+      done(null, people)
+  })
+  
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({
+    name: personName
+  }, (err, personFound) => {
+    if(err) return console.log(err)
+      done(null, personFound);
+  })
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if(err) return console.log(err)
+      done(null, data)
+  })
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
-};
+  Person.findById(personId, (err, data) => {
+    if(err) return console.log(err)
+      done(null, data)
+  })
+
+  pe
+}
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err)
 
-  done(null /*, data*/);
+      person.favoriteFoods.push(foodToAdd);
+      
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err)
+        done(null, updatedPerson)
+    })
+  })
 };
 
+// findOneAndUpdate uses ( conditions , update , options , callback ) as arguments.
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedDoc) => {
+    if(err) return console.log(err)
+      done(null, updatedDoc)
+  } )
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, removedDoc) => {
+    if(err) return console.log(err)
+      done(null, removedDoc)
+  })
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, (err, response) => {
+    if(err) return console.log(err)
+      done(null, response)
+  })
 };
 
-const queryChain = (done) => {
+const queryChain = function(done) {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: -1 })          // sort alphabetically
+    .limit(2)                   // limit results to 2
+    .select({ age: 0 })         // exclude the age field
+    .exec(function(err, data) {
+      if (err) return done(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
---
+++ src/database.js
@@ -0,0 +1,24 @@
+// let mongoose = require('mongoose');
+
+// const server =  '136.158.26.19/32'
+
+// const database = 'babyplaya'; // REPLACE WITH YOUR DB NAME
+
+// class Database {
+//   constructor() {
+//     this._connect();
+//   }
+
+//   _connect() {
+//     mongoose
+//       .connect(`mongodb://${server}/${database}`)
+//       .then(() => {
+//         console.log('Database connection successful');
+//       })
+//       .catch((err) => {
+//         console.error('Database connection error');
+//       });
+//   }
+// }
+
+// module.exports = new Database();
